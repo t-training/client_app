8593985cd71ab9f901a3594aff47b6be291c9a37
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-12 12:28:12 +0900

    micropost_iframesでAPIを取得して返すアクションを作成

--- app/controllers/micropost_iframes_controller.rb
+++ app/controllers/micropost_iframes_controller.rb
@@ -1,5 +1,12 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class MicropostIframesController < ApplicationController
   def show_microposts
+    uri = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=140010')
+    microposts_json = Net::HTTP.get(uri)
+    @microposts_hash = JSON.parse(microposts_json)
     render :layout => nil
   end
 end
