caef04ea02bdba6a750e509d59bc14775569a371
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-12 14:57:59 +0900

    micropostのcontentとcreated_atを表示するようにした

--- app/controllers/micropost_iframes_controller.rb
+++ app/controllers/micropost_iframes_controller.rb
@@ -4,9 +4,14 @@ require 'json'
 
 class MicropostIframesController < ApplicationController
   def index
+=begin 
     uri = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=140010')
     microposts_json = Net::HTTP.get(uri)
     @microposts_hash = JSON.parse(microposts_json)
+=end
+    File.open("#{Rails.public_path}/json/microposts.json") do |j|
+      @microposts_hash = JSON.load(j)
+    end
     render :layout => nil
   end
 end
