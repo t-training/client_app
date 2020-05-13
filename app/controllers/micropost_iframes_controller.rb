bea96e6c58078122b65bd810eae20e1ff2c2d7ca
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-13 12:56:46 +0900

    本番APIから取得するURIに修正し、表示サイズを変更

--- app/controllers/micropost_iframes_controller.rb
+++ app/controllers/micropost_iframes_controller.rb
@@ -3,17 +3,16 @@ require 'uri'
 require 'json'
 
 class MicropostIframesController < ApplicationController
-  def index
-=begin 
-    uri = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=140010')
+  def index 
+    uri = URI.parse('https://afternoon-anchorage-19414.herokuapp.com/api/v1/users/2/get_microposts')
     microposts_json = Net::HTTP.get(uri)
     @microposts_hash = JSON.parse(microposts_json)
-=end
 
+=begin 
     File.open("#{Rails.public_path}/json/microposts.json") do |j|
       @microposts_hash = JSON.load(j)
     end
-
+=end
     render :layout => nil
   end
 end
