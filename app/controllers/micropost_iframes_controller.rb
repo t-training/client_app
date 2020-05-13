b1178a1bbe325c1dec4d0d764318683558712dda
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-13 10:38:40 +0900

    テストのpath指定が間違っていたので修正

--- app/controllers/micropost_iframes_controller.rb
+++ app/controllers/micropost_iframes_controller.rb
@@ -4,14 +4,16 @@ require 'json'
 
 class MicropostIframesController < ApplicationController
   def index
-    uri = URI.parse('')
+=begin 
+    uri = URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=140010')
     microposts_json = Net::HTTP.get(uri)
     @microposts_hash = JSON.parse(microposts_json)
-=begin 
+=end
+
     File.open("#{Rails.public_path}/json/microposts.json") do |j|
       @microposts_hash = JSON.load(j)
     end
-=end
+
     render :layout => nil
   end
 end
--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "StaticPages", type: :system do
   describe `root` do
     before { visit root_path }
     it `is root page` do
-      expect(page).to have_current_path ""
+      expect(page).to have_current_path root_path
     end
   end
 end
