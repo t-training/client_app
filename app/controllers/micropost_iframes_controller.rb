109dd3199f8ea980be2f5c7e6fd51e1e5121fb4f
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-13 14:22:47 +0900

    テスト用に使っていたjsonファイルの削除とjsonファイル読み込みコードのコメントアウトを削除

--- app/controllers/micropost_iframes_controller.rb
+++ app/controllers/micropost_iframes_controller.rb
@@ -7,12 +7,6 @@ class MicropostIframesController < ApplicationController
     uri = URI.parse('https://afternoon-anchorage-19414.herokuapp.com/api/v1/users/2/get_microposts')
     microposts_json = Net::HTTP.get(uri)
     @microposts_hash = JSON.parse(microposts_json)
-
-=begin 
-    File.open("#{Rails.public_path}/json/microposts.json") do |j|
-      @microposts_hash = JSON.load(j)
-    end
-=end
     render :layout => nil
   end
 end
