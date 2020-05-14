7671877e15dc1d61f4eff4970eeb137c1c475ee7
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-14 11:55:45 +0900

    micropost_iframesのmicropost受け取りの変数名変更とそのindexでの表示に使う変数名変更

--- app/controllers/micropost_iframes_controller.rb
+++ app/controllers/micropost_iframes_controller.rb
@@ -5,8 +5,8 @@ require 'json'
 class MicropostIframesController < ApplicationController
   def index 
     uri = URI.parse('https://afternoon-anchorage-19414.herokuapp.com/api/v1/users/2/get_microposts')
-    microposts_json = Net::HTTP.get(uri)
-    @microposts_hash = JSON.parse(microposts_json)
+    response = Net::HTTP.get(uri)
+    @microposts = JSON.parse(response)
     render :layout => nil
   end
 end
