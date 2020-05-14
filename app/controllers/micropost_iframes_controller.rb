5bdd053e71b028a77a758f15392adb433ef9e20f
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-14 18:03:29 +0900

    micropost_iframeにあるrenderの書き方を変更

--- app/controllers/micropost_iframes_controller.rb
+++ app/controllers/micropost_iframes_controller.rb
@@ -7,6 +7,6 @@ class MicropostIframesController < ApplicationController
     uri = URI.parse('https://afternoon-anchorage-19414.herokuapp.com/api/v1/users/2/microposts')
     response = Net::HTTP.get(uri)
     @microposts = JSON.parse(response)
-    render :layout => nil
+    render layout: nil
   end
 end
