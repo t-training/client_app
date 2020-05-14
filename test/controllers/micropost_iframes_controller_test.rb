82c933c539fc50be124132bc751c842a93deac62
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-14 17:39:38 +0900

    minitest用のファイルを削除してminitestファイルを生成しないようにした

--- test/controllers/micropost_iframes_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MicropostIframesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show_microposts" do
-    get micropost_iframes_show_microposts_url
-    assert_response :success
-  end
-
-end
