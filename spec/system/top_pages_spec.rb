da990690cc618ffa5f911c221a78de152f604208
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-15 16:19:26 +0900

    api部分のrspecをtop_pagesのrspecから削除

--- spec/system/top_pages_spec.rb
+++ spec/system/top_pages_spec.rb
@@ -8,30 +8,4 @@ RSpec.describe "TopPages", type: :system do
       expect(page).to have_current_path root_path
     end
   end
-
-  describe `microposts` do
-    let!(:iframe) { find('iframe') }
-    context `MicropostAPI` do
-      it `returns user_icon` do
-        within_frame(iframe) do
-          is_expected.to have_selector(".gravatar")
-        end
-      end
-      it `returns user_name` do
-        within_frame(iframe) do
-          is_expected.to have_selector(".user")
-        end
-      end
-      it `returns user_post` do
-        within_frame(iframe) do
-          is_expected.to have_selector(".content")
-        end
-      end
-      it `returns post time` do
-        within_frame(iframe) do
-          is_expected.to have_selector(".timestamp")
-        end
-      end
-    end
-  end
 end
