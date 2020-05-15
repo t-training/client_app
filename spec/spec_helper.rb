0f4a6b2511318e8d92312cc403b973d6f6258c70
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-15 17:50:59 +0900

    webmockのgemインストール

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'capybara/rspec'
+require 'webmock/rspec'
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :selenium_chrome_headless
