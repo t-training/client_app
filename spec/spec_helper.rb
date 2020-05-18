d3bd7782d16837cc304b2347ade5723f350e890a
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-18 12:04:56 +0900

    webmockの記法を修正 #7

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,10 +21,10 @@ RSpec.configure do |config|
   end
   config.before(:each) do
     WebMock.allow_net_connect!
-    WebMock.stub_request(:get, "https://afternoon-anchorage-19414.herokuapp.com/api/v1/users/2/microposts").to_return(
+    stub_request(:get, "https://afternoon-anchorage-19414.herokuapp.com/api/v1/users/2/microposts").to_return(
       body: File.read("#{Rails.root}/test/fixtures/user1_microposts.json"),
       status: 200,
-      headers: { 'Content-Type' =>  'application/json' })
+      headers: { 'Content-Type' => 'application/json' })
   end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
