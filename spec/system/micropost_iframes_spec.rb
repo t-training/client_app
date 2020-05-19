0e24d88007cdfe7bec209eab5db02e64dfef4628
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-19 11:21:08 +0900

    存在しないuserのidを指定した時のsystem spec

--- spec/system/micropost_iframes_spec.rb
+++ spec/system/micropost_iframes_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe "MicropostIframes", type: :system do
       is_expected.to have_selector(".content")
     end
   end
+  
+  context `not exist user` do
+    before { visit "/microposts?id=999" }
+    it `returns error title` do
+      is_expected.to have_selector("h2", text: "エラー")
+    end
+    it `returns error message` do
+      is_expected.to have_content("Validation Failed")
+    end
+  end
+  
 end
