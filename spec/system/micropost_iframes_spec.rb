f51630198fb510b3f0ce08edc05670f1b9b3dc10
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-19 11:12:19 +0900

    userが存在する時のsystem rspec

---
+++ spec/system/micropost_iframes_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "MicropostIframes", type: :system do
+  subject { page }
+
+  context `exist user` do
+    before { visit "/microposts?id=1" }
+    it `returns user_name title` do
+      is_expected.to have_selector("h2", text: "Example Userのpost")
+    end
+    it `returns microposts` do
+      is_expected.to have_selector(".content")
+    end
+  end
+end
