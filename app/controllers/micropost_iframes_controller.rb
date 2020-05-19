fa0fe23e9c5f30e42e1c22a1663905909c666286
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-19 18:05:30 +0900

    ステータスコードに応じたレンダリング

--- app/controllers/micropost_iframes_controller.rb
+++ app/controllers/micropost_iframes_controller.rb
@@ -5,8 +5,13 @@ require 'json'
 class MicropostIframesController < ApplicationController
   def index 
     uri = URI.parse("https://afternoon-anchorage-19414.herokuapp.com/api/v1/users/#{params[:id]}/microposts")
-    response = Net::HTTP.get(uri)
-    @microposts = JSON.parse(response)
-    render layout: nil
+    response = Net::HTTP.get_response(uri)
+    if (response.code == "200")
+      @microposts = JSON.parse(response.body)
+      render partial: 'microposts'
+    else
+      @error = JSON.parse(response.body)
+      render partial: 'error_microposts'
+    end
   end
 end
