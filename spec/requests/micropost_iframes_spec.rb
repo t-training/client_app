53f89556125c34e98d4ad7a4b863ca77f8eb158c
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-19 19:12:29 +0900

    ユーザーが存在しない時のエラーのrequest specを追加

--- spec/requests/micropost_iframes_spec.rb
+++ spec/requests/micropost_iframes_spec.rb
@@ -5,8 +5,18 @@ RSpec.describe "MicropostIframes", type: :request do
     subject { response.body }
 
     it `returns json` do
-      get microposts_path
+      get microposts_path id:1
       is_expected.to include "Example User"
+    end
+
+    context `not exist user` do
+      before { get microposts_path id:999 }
+      it `returns error title` do
+        is_expected.to include "エラー"
+      end
+      it `returns error message` do
+        is_expected.to include "Validation Failed"
+      end
     end
   end
 end
