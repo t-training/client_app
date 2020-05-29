765f66bec84943fe10bd7c356f3c34c27d0a0c8a
take-ishii <t_ishii@timedia.co.jp>
2020-05-29 07:43:35 +0000

    ログインリンク（後々フォローリンクになる）を追加

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,4 +1,5 @@
 class TopPagesController < ApplicationController
   def index
+    @url = "https://afternoon-anchorage-19414.herokuapp.com/login/?url=#{root_url}"
   end
 end
--- spec/system/top_pages_spec.rb
+++ spec/system/top_pages_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe "TopPages", type: :system do
       expect(page).to have_current_path root_path
     end
   end
+  
+  describe 'outside login' do
+    before do
+      visit root_path
+      click_link "Log in"
+      fill_in "Email", with: "example@railstutorial.org"
+      fill_in "Password", with: "foobar"
+      click_button "Log in"
+    end
+    
+    scenario "リダイレクトURLが指定したURLであること" do
+      expect(page).to have_current_path(root_path, ignore_query: true)
+    end
+    scenario "user_idとtokenがURLに含まれていること" do
+      query_hash = Rack::Utils.parse_nested_query(URI.parse(current_url).query)
+      expect(query_hash['user_id']).to_not be_empty
+      expect(query_hash['token']).to_not be_empty
+    end
+  end
 end
