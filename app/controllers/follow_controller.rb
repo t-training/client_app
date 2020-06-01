ababeb0a689e27d9ae4ff40bd05dafea1fab4d5c
take-ishii <t_ishii@timedia.co.jp>
2020-06-01 09:17:33 +0000

    CSRF対策の対策

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -3,6 +3,8 @@ require 'uri'
 require 'json'
 
 class FollowController < ApplicationController
+  protect_from_forgery :except => [:create]
+
   def create 
     #REVIEW: 未テスト
     uri = URI.parse("https://afternoon-anchorage-19414.herokuapp.com/api/v1/relationships")
@@ -15,7 +17,7 @@ class FollowController < ApplicationController
     req = Net::HTTP::Post.new(uri.path)
     req.set_form_data(request_params)
     req.initialize_http_header({ "Content-Type" => "application/json"})
-    req.initialize_http_header({ "Authorization" => "Token " + cookies.permanent[:access_token]})
+    req.initialize_http_header({ "Authorization" => "Token #{cookies.permanent[:access_token]}"})
     
     response = http.request(req)
 
