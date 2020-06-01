71050517ec60588f545126620412041f00d81fbb
take-ishii <t_ishii@timedia.co.jp>
2020-06-01 09:18:50 +0000

    フォローボタン追加とルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/microposts', to: 'micropost_iframes#index'
+  post '/follow', to: 'follow#create'
   root 'top_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
