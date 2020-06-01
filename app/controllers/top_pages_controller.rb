d3d2b11458ce774ec0b38cba4b4fb0fb4217d54e
take-ishii <t_ishii@timedia.co.jp>
2020-06-01 02:07:46 +0000

    Cookieにuser_idとtokenを保存

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,5 +1,10 @@
 class TopPagesController < ApplicationController
   def index
+    if(params[:user_id] && params[:token]) 
+      cookies.permanent.signed[:user_id] = params[:user_id]
+      cookies.permanent[:access_token] = params[:token]
+    end
+    
     @url = "https://afternoon-anchorage-19414.herokuapp.com/login/?url=#{root_url}"
   end
 end
