91c847da3dcd3b5052c69c6b765df70c29a8111e
take-ishii <t_ishii@timedia.co.jp>
2020-06-03 05:37:12 +0000

    単体テスト追加　RED

--- spec/system/top_pages_spec.rb
+++ spec/system/top_pages_spec.rb
@@ -10,21 +10,43 @@ RSpec.describe "TopPages", type: :system do
   end
   
   describe 'outside login' do
-    before do
-      visit root_path
-      click_link "Log in"
-      fill_in "Email", with: "example@railstutorial.org"
-      fill_in "Password", with: "foobar"
-      click_button "Log in"
+    describe '遷移前' do
+      before do
+        visit root_path
+        click_link "Log in"
+      end
+      
+      scenario 'URL付きでリダイレクトされること' do
+        expect(page).to have_current_path("https://afternoon-anchorage-19414.herokuapp.com/login/", ignore_query: true)
+        query_hash = Rack::Utils.parse_nested_query(URI.parse(current_url).query)
+        expect(query_hash['url']).to_not be_empty
+      end
     end
-    
-    scenario "リダイレクトURLが指定したURLであること" do
-      expect(page).to have_current_path(root_path, ignore_query: true)
-    end
-    scenario "user_idとtokenがURLに含まれていること" do
-      query_hash = Rack::Utils.parse_nested_query(URI.parse(current_url).query)
-      expect(query_hash['user_id']).to_not be_empty
-      expect(query_hash['token']).to_not be_empty
+
+    describe '遷移後' do
+      # TODO: user_idとtokenはランダム生成できると良さげ
+      let(:user_id) { "1" }
+      let(:token) { "aaaaa" }
+      
+      before do
+        uri = URI(root_url)
+        uri.query = URI.encode_www_form({user_id: user_id, token: token})
+        visit uri
+      end
+      scenario "user_idとtokenがURLに含まれていること" do
+        query_hash = Rack::Utils.parse_nested_query(URI.parse(current_url).query)
+        expect(query_hash['user_id']).to_not be_empty
+        expect(query_hash['token']).to_not be_empty
+      end
+      scenario "cookiesに保存されていること" do
+        show_me_the_cookies
+        cookies = page.driver.browser.manage.all_cookies
+        token_cookie = cookies.find { |c| c[:name] == "access_token" }
+        id_cookie = cookies.find { |c| c[:name] == "user_id" }
+        expect(token_cookie[:value]).to eq token
+        # FIXME: id_cookieは署名付きクッキーなので合わない
+        expect(id_cookie[:value]).to eq user_id
+      end
     end
   end
 end
