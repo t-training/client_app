0d083bd7635d9a8e75170a338c9987cb54b45305
take-ishii <t_ishii@timedia.co.jp>
2020-06-04 02:56:25 +0000

    リクエスト生成をメソッド化

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -4,21 +4,15 @@ require 'json'
 
 class FollowController < ApplicationController
   protect_from_forgery :except => [:create]
+  include FollowHelper
   
   def create 
     #REVIEW: 未テスト
     uri = URI.parse("https://afternoon-anchorage-19414.herokuapp.com/api/v1/relationships")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = uri.scheme === "https"
-    
-    request_params = { user_id: cookies.permanent.signed[:user_id], 
-               followed_id: params[:followed_id] }
-    
-    req = Net::HTTP::Post.new(uri.path)
-    req.set_form_data(request_params)
-    req.initialize_http_header({ "Content-Type" => "application/json"})
-    req.initialize_http_header({ "Authorization" => "Token #{cookies.permanent[:access_token]}"})
-    
+    req = update_request(uri, cookies.permanent.signed[:user_id], params[:followed_id], cookies.permanent[:access_token])
+
     response = http.request(req)
     
     response_json = JSON.parse(response.body)
