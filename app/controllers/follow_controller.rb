d88aff361894409a28c892648f73a87a0b48b370
take-ishii <t_ishii@timedia.co.jp>
2020-06-04 05:26:25 +0000

    エラー処理とそれぞれの場合のメッセージ追加

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -13,18 +13,25 @@ class FollowController < ApplicationController
     http.use_ssl = uri.scheme === "https"
     req = update_request(uri, params[:followed_id], cookies.permanent[:access_token])
 
-    response = http.request(req)
-    
-    response_json = JSON.parse(response.body)
-    
-    if(!response_json["is_logged"])
-      uri = URI("https://afternoon-anchorage-19414.herokuapp.com/login/")
-      uri.query = URI.encode_www_form({url: root_url})
-      redirect_to uri.to_s
-    elsif(!reponse_json["followed"])
-      #TODO: 既にフォローしていたことを伝えるメッセージ表示
-    elsif(response_json["followed"]) 
-      #TODO: フォローできたことを伝えるメッセージ表示
+    begin
+      response = http.request(req)
+      
+      response_json = JSON.parse(response.body)
+      
+      if(response.code == "200")
+        if(!reponse_json["followed"])
+          flash[:info] = "既にフォローしています"
+        elsif(response_json["followed"]) 
+          flash[:success] = "フォローできました"
+        end
+      else
+        uri = URI("https://afternoon-anchorage-19414.herokuapp.com/login/")
+        uri.query = URI.encode_www_form({url: root_url})
+        redirect_to uri.to_s
+      end
+    rescue
+      flash[:danger] = "アクセスエラー"
+      redirect_to root_url
     end
   end
 end
