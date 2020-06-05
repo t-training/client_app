8f70e6c514ba85ff220e831003d75902b6d85e60
take-ishii <t_ishii@timedia.co.jp>
2020-06-05 05:25:56 +0000

    beginを削除

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -8,27 +8,25 @@ class FollowController < ApplicationController
 
   def create 
     #REVIEW: 未テスト
-    begin
-      response = RestClient.post "https://afternoon-anchorage-19414.herokuapp.com/api/v1/users/#{cookies.permanent.signed[:user_id]}/relationships",
-                      {params: {:followed_id => params[:followed_id]}}, 
-                      {:Content-Type => "application/json", :Authorization => "Token #{cookies.permanent[:access_token]}"}
-      
-      response_json = JSON.parse(response.body)
-      
-      if(response.code == "200")
-        if(!reponse_json["followed"])
-          flash[:info] = "既にフォローしています"
-        elsif(response_json["followed"]) 
-          flash[:success] = "フォローできました"
-        end
-      else
-        uri = URI("https://afternoon-anchorage-19414.herokuapp.com/login/")
-        uri.query = URI.encode_www_form({url: root_url})
-        redirect_to uri.to_s
+    response = RestClient.post "https://afternoon-anchorage-19414.herokuapp.com/api/v1/users/#{cookies.permanent.signed[:user_id]}/relationships",
+                    {params: {:followed_id => params[:followed_id]}}, 
+                    {:Content-Type => "application/json", :Authorization => "Token #{cookies.permanent[:access_token]}"}
+    
+    response_json = JSON.parse(response.body)
+    
+    if(response.code == "200")
+      if(!reponse_json["followed"])
+        flash[:info] = "既にフォローしています"
+      elsif(response_json["followed"]) 
+        flash[:success] = "フォローできました"
       end
-    rescue
-      flash[:danger] = "アクセスエラー"
-      redirect_to root_url
+    else
+      uri = URI("https://afternoon-anchorage-19414.herokuapp.com/login/")
+      uri.query = URI.encode_www_form({url: root_url})
+      redirect_to uri.to_s
     end
+  rescue
+    flash[:danger] = "アクセスエラー"
+    redirect_to root_url
   end
 end
